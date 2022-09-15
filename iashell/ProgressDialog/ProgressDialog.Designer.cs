8887c3751ed257f08ed6e6e36d5af6e57311e02f
H497222 <iain.ferguson@honeywell.com>
2022-09-15 11:43:45 +0100

    changed

--- iashell/ProgressDialog/ProgressDialog.Designer.cs
+++ iashell/ProgressDialog/ProgressDialog.Designer.cs
@@ -46,9 +46,10 @@
             // ProcessingLabel
             // 
             this.ProcessingLabel.AutoSize = true;
-            this.ProcessingLabel.Location = new System.Drawing.Point(12, 9);
+            this.ProcessingLabel.Location = new System.Drawing.Point(18, 14);
+            this.ProcessingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ProcessingLabel.Name = "ProcessingLabel";
-            this.ProcessingLabel.Size = new System.Drawing.Size(84, 13);
+            this.ProcessingLabel.Size = new System.Drawing.Size(125, 20);
             this.ProcessingLabel.TabIndex = 0;
             this.ProcessingLabel.Text = "Processing item:";
             // 
@@ -56,61 +57,69 @@
             // 
             this.ActionLabel.AutoSize = true;
             this.ActionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ActionLabel.Location = new System.Drawing.Point(12, 30);
+            this.ActionLabel.Location = new System.Drawing.Point(18, 46);
+            this.ActionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ActionLabel.Name = "ActionLabel";
-            this.ActionLabel.Size = new System.Drawing.Size(158, 17);
+            this.ActionLabel.Size = new System.Drawing.Size(220, 25);
             this.ActionLabel.TabIndex = 1;
             this.ActionLabel.Text = "Paused - 68% complete";
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(12, 60);
+            this.progressBar.Location = new System.Drawing.Point(18, 92);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(410, 23);
+            this.progressBar.Size = new System.Drawing.Size(1442, 35);
             this.progressBar.TabIndex = 2;
             // 
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(12, 99);
+            this.nameLabel.Location = new System.Drawing.Point(18, 152);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(38, 13);
+            this.nameLabel.Size = new System.Drawing.Size(55, 20);
             this.nameLabel.TabIndex = 3;
             this.nameLabel.Text = "Name:";
             // 
             // timeRemainingLabel
             // 
             this.timeRemainingLabel.AutoSize = true;
-            this.timeRemainingLabel.Location = new System.Drawing.Point(102, 116);
+            this.timeRemainingLabel.Location = new System.Drawing.Point(153, 178);
+            this.timeRemainingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.timeRemainingLabel.Name = "timeRemainingLabel";
-            this.timeRemainingLabel.Size = new System.Drawing.Size(35, 13);
+            this.timeRemainingLabel.Size = new System.Drawing.Size(52, 20);
             this.timeRemainingLabel.TabIndex = 4;
             this.timeRemainingLabel.Text = "About";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 133);
+            this.label3.Location = new System.Drawing.Point(18, 205);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(58, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Action:";
             // 
             // outputList
             // 
             this.outputList.FormattingEnabled = true;
-            this.outputList.Location = new System.Drawing.Point(12, 188);
+            this.outputList.ItemHeight = 20;
+            this.outputList.Location = new System.Drawing.Point(22, 248);
+            this.outputList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.outputList.Name = "outputList";
             this.outputList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.outputList.Size = new System.Drawing.Size(410, 212);
+            this.outputList.Size = new System.Drawing.Size(1442, 384);
             this.outputList.TabIndex = 6;
             // 
             // buttonCancel
             // 
             this.buttonCancel.Image = global::ProgressDialog.Properties.Resources.cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(401, 30);
+            this.buttonCancel.Location = new System.Drawing.Point(1425, 46);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(21, 21);
+            this.buttonCancel.Size = new System.Drawing.Size(32, 32);
             this.buttonCancel.TabIndex = 7;
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
@@ -118,34 +127,38 @@
             // labelItemText
             // 
             this.labelItemText.AutoSize = true;
-            this.labelItemText.Location = new System.Drawing.Point(101, 9);
+            this.labelItemText.Location = new System.Drawing.Point(152, 14);
+            this.labelItemText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelItemText.Name = "labelItemText";
-            this.labelItemText.Size = new System.Drawing.Size(37, 13);
+            this.labelItemText.Size = new System.Drawing.Size(49, 20);
             this.labelItemText.TabIndex = 8;
             this.labelItemText.Text = "          ";
             // 
             // labelImageName
             // 
             this.labelImageName.AutoSize = true;
-            this.labelImageName.Location = new System.Drawing.Point(56, 99);
+            this.labelImageName.Location = new System.Drawing.Point(84, 152);
+            this.labelImageName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelImageName.Name = "labelImageName";
-            this.labelImageName.Size = new System.Drawing.Size(0, 13);
+            this.labelImageName.Size = new System.Drawing.Size(0, 20);
             this.labelImageName.TabIndex = 9;
             // 
             // labelAction
             // 
             this.labelAction.AutoSize = true;
-            this.labelAction.Location = new System.Drawing.Point(12, 148);
+            this.labelAction.Location = new System.Drawing.Point(18, 228);
+            this.labelAction.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelAction.Name = "labelAction";
-            this.labelAction.Size = new System.Drawing.Size(0, 13);
+            this.labelAction.Size = new System.Drawing.Size(0, 20);
             this.labelAction.TabIndex = 10;
             // 
             // buttonPause
             // 
             this.buttonPause.Image = global::ProgressDialog.Properties.Resources.running;
-            this.buttonPause.Location = new System.Drawing.Point(351, 30);
+            this.buttonPause.Location = new System.Drawing.Point(1385, 46);
+            this.buttonPause.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonPause.Name = "buttonPause";
-            this.buttonPause.Size = new System.Drawing.Size(21, 21);
+            this.buttonPause.Size = new System.Drawing.Size(32, 32);
             this.buttonPause.TabIndex = 11;
             this.buttonPause.UseVisualStyleBackColor = true;
             this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
@@ -153,17 +166,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 116);
+            this.label1.Location = new System.Drawing.Point(18, 178);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.Size = new System.Drawing.Size(124, 20);
             this.label1.TabIndex = 12;
             this.label1.Text = "Time remaining: ";
             // 
             // ProgressDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 443);
+            this.ClientSize = new System.Drawing.Size(1485, 758);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonPause);
             this.Controls.Add(this.labelAction);
@@ -177,6 +191,7 @@
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.ActionLabel);
             this.Controls.Add(this.ProcessingLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ProgressDialog";
             this.Text = "Import";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ProgressDialog_FormClosing);
