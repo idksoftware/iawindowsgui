67d359764bd7c2cd82a4e22d89bd6b4ee28ace8b
H497222 <iain.ferguson@honeywell.com>
2022-10-03 10:14:39 +0100

    added ExportSingleForm

---
+++ iashell/iaexport/ExportSingleForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using iaforms;
+using System.Windows.Forms;
+using System.IO;
+
+namespace iaexport
+{
+    public partial class ExportSingleForm : Form
+    {
+        string m_file;
+        public ExportSingleForm(string file)
+        {
+            InitializeComponent();
+            m_file = file;
+            FileInfo fileItem = new FileInfo(file);
+            labelType.Text = fileItem.Extension;
+            DateTime lastmodified = fileItem.LastWriteTime;
+            string dateString = lastmodified.ToString("HH:mm MM/dd/yyyy");
+            labelDateModified.Text = dateString;
+            labelFolder.Text = fileItem.DirectoryName;
+            labelImage.Text = fileItem.Name;
+            labelSize.Text = fileItem.Length.ToString();
+        }
+
+        private void buttonProperties_Click(object sender, EventArgs e)
+        {
+            (new PropertiesForm(m_file)).Show();
+        }
+    }
+}
