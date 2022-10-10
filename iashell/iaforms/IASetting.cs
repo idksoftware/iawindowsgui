661ff0eaee3c0a73e5aaa4cd7e5a5873cde912de
Iain Ferguson <i.ferguson@idk.co.uk>
2022-10-10 12:14:21 +0100

    IASettings

---
+++ iashell/iaforms/IASetting.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace iaforms
+{
+    
+    internal class IASetting
+    {
+        string m_exePath;
+        string m_workingPath; public delegate void ProgressEventHandler();
+        XmlTextReader reader = null;
+        XmlDocument document = new XmlDocument();
+
+        public static ProgressEventHandler ProgressChanged;
+        async Task<bool> readAsync(string exePath, string workingPath)
+        {
+            m_exePath = exePath;
+            m_workingPath = workingPath;
+            
+            LaunchCommandLine readIAAdmin = LaunchCommandLine.Instance;
+            readIAAdmin.ExePath = m_exePath;
+            readIAAdmin.Path = m_workingPath;
+
+            LaunchCommandLine.StatusChanged += OnStatusChanged;
+            ProgressChanged += OnProgressChanged;
+            readIAAdmin.Arguments = "checkin --scope=\"";
+            await readIAAdmin.LaunchCommand();
+
+            return true;
+        }
+
+        private void OnProgressChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnStatusChanged(LaunchCommandLine.Status param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
