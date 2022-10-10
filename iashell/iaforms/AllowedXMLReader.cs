431219d1a7e2e8fc0c3a43fd4af5f37f0f40f7b9
Iain Ferguson <i.ferguson@idk.co.uk>
2022-10-10 12:13:42 +0100

    added AllowedXMLReader.cs

---
+++ iashell/iaforms/AllowedXMLReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iaforms
+{
+    class AllowedXMLReader
+    {
+        public class AllowItem
+        {
+            public enum AllowItemType
+            {
+                Picture,
+                Raw,
+                Unknown
+            }
+            public string Ext;
+            public AllowItemType type;
+            public string mime;
+            public string discription;
+        }
+
+
+    }
+}
