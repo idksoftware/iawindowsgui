4049ff7e8afb48d18230857af062b61f60212b6e
Iain Ferguson <i.ferguson@idk.co.uk>
2024-11-27 20:29:36 +0000

    added ImportProgressForm

---
+++ iashell/iaimport/ImportProgressForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace iaimport
+{
+    partial class ImportProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportProgressForm));
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.ActionLabel = new System.Windows.Forms.Label();
+            this.labelProcess = new System.Windows.Forms.Label();
+            this.selectedItems = new System.Windows.Forms.ListView();
+            this.ItemColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timeRemainingLabel = new System.Windows.Forms.Label();
+            this.name3 = new System.Windows.Forms.Label();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Image = ((System.Drawing.Image)(resources.GetObject("buttonPause.Image")));
+            this.buttonPause.Location = new System.Drawing.Point(740, 19);
+            this.buttonPause.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(32, 32);
+            this.buttonPause.TabIndex = 35;
+            this.buttonPause.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Image = ((System.Drawing.Image)(resources.GetObject("buttonCancel.Image")));
+            this.buttonCancel.Location = new System.Drawing.Point(788, 19);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(32, 32);
+            this.buttonCancel.TabIndex = 34;
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(18, 67);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(802, 35);
+            this.progressBar.TabIndex = 33;
+            // 
+            // ActionLabel
+            // 
+            this.ActionLabel.AutoSize = true;
+            this.ActionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActionLabel.Location = new System.Drawing.Point(13, 21);
+            this.ActionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ActionLabel.Name = "ActionLabel";
+            this.ActionLabel.Size = new System.Drawing.Size(220, 25);
+            this.ActionLabel.TabIndex = 32;
+            this.ActionLabel.Text = "Paused - 68% complete";
+            // 
+            // labelProcess
+            // 
+            this.labelProcess.AutoSize = true;
+            this.labelProcess.Location = new System.Drawing.Point(103, 171);
+            this.labelProcess.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelProcess.Name = "labelProcess";
+            this.labelProcess.Size = new System.Drawing.Size(52, 20);
+            this.labelProcess.TabIndex = 41;
+            this.labelProcess.Text = "About";
+            // 
+            // selectedItems
+            // 
+            this.selectedItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ItemColumn,
+            this.nameColumn});
+            this.selectedItems.HideSelection = false;
+            this.selectedItems.Location = new System.Drawing.Point(18, 207);
+            this.selectedItems.Name = "selectedItems";
+            this.selectedItems.Size = new System.Drawing.Size(802, 373);
+            this.selectedItems.TabIndex = 40;
+            this.selectedItems.UseCompatibleStateImageBehavior = false;
+            this.selectedItems.View = System.Windows.Forms.View.Details;
+            // 
+            // ItemColumn
+            // 
+            this.ItemColumn.Text = "Item";
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.Text = "Image";
+            this.nameColumn.Width = 350;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 143);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Time remaining: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 170);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Process:";
+            // 
+            // timeRemainingLabel
+            // 
+            this.timeRemainingLabel.AutoSize = true;
+            this.timeRemainingLabel.Location = new System.Drawing.Point(136, 143);
+            this.timeRemainingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timeRemainingLabel.Name = "timeRemainingLabel";
+            this.timeRemainingLabel.Size = new System.Drawing.Size(52, 20);
+            this.timeRemainingLabel.TabIndex = 37;
+            this.timeRemainingLabel.Text = "About";
+            // 
+            // name3
+            // 
+            this.name3.AutoSize = true;
+            this.name3.Location = new System.Drawing.Point(14, 116);
+            this.name3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.name3.Name = "name3";
+            this.name3.Size = new System.Drawing.Size(55, 20);
+            this.name3.TabIndex = 36;
+            this.name3.Text = "Name:";
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDone.Location = new System.Drawing.Point(702, 591);
+            this.buttonDone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(118, 45);
+            this.buttonDone.TabIndex = 42;
+            this.buttonDone.Text = "Done";
+            this.buttonDone.UseVisualStyleBackColor = true;
+            // 
+            // ImportProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 650);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.labelProcess);
+            this.Controls.Add(this.selectedItems);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timeRemainingLabel);
+            this.Controls.Add(this.name3);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.ActionLabel);
+            this.Name = "ImportProgressForm";
+            this.Text = "Import Progress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label ActionLabel;
+        private System.Windows.Forms.Label labelProcess;
+        private System.Windows.Forms.ListView selectedItems;
+        private System.Windows.Forms.ColumnHeader ItemColumn;
+        private System.Windows.Forms.ColumnHeader nameColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label timeRemainingLabel;
+        private System.Windows.Forms.Label name3;
+        private System.Windows.Forms.Button buttonDone;
+    }
+}
---
+++ iashell/iaimport/ImportProgressForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iaforms;
+
+namespace iaimport
+{
+    public partial class ImportProgressForm : Form
+    {
+        public delegate void ProgressEventHandler();
+        public static ProgressEventHandler ProgressChanged;
+        String m_timeRemaining;
+        List<string> m_files;
+        string m_exePath;
+        string m_workingPath;
+        string m_currentFile;
+        public ImportProgressForm(string folder, string exePath, string workingPath)
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            //m_files = folder;
+            m_exePath = exePath;
+            m_workingPath = workingPath;
+            this.labelProcess.Text = "Check-out";
+            this.Name = "Check-out Calculating...";
+
+            /*
+            progressBar.Maximum = 100;
+            AddItems(m_files);
+            ActionLabel.Text = progressBar.Value + "% complete";
+            timeRemainingLabel.Text = "Calculating...";
+            timerStartProcess.Start();
+            */
+        }
+
+        public async Task<int> ProgressData()
+        {
+
+            float step = 100 / m_files.Count;
+            float progress;
+
+            LaunchCommandLine launchCommandLine = LaunchCommandLine.Instance;
+            launchCommandLine.ExePath = m_exePath;
+            launchCommandLine.Path = m_workingPath;
+
+            LaunchCommandLine.StatusChanged += OnStatusChanged;
+            ProgressChanged += OnProgressChanged;
+
+            this.progressBar.Maximum = 100;
+            TimeSpan elapsed;
+            //await Task.Delay(2000);
+            int idx = 0;
+            int itemsToByProcessed = m_files.Count;
+            ActionLabel.Text = progressBar.Value + "% complete";
+            timeRemainingLabel.Text = "Calculating...";
+            
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            timer.Restart();
+            string address;
+            string name;
+            //GetAddress(file, out address);
+            //GetFile(file, out name);
+            //m_currentFile = name;
+            //string fileAddress = address + '/' + name;
+            //launchCommandLine.FilePath = fileAddress;
+            //launchCommandLine.Arguments = "checkout --scope=\"" + fileAddress + "\"";
+            await launchCommandLine.LaunchCommand();
+
+            LaunchCommandLine.ExitCode exitCode = launchCommandLine.ProcessExitCode;
+            //labelComment.Text = launchCommandLine.ProcessReturnString;
+            selectedItems.BeginUpdate();
+            switch (exitCode)
+            {
+                case LaunchCommandLine.ExitCode.Success:
+                    selectedItems.Items[idx].ImageIndex = 1;
+                    //labelResult.Text = "Success";
+                    //labelComment.Text = "Successfully checked out: " + name;
+                    break;
+                case LaunchCommandLine.ExitCode.Warnings:
+                    selectedItems.Items[idx].ImageIndex = 2;
+                    //labelResult.Text = "Warnings";
+                    break;
+                case LaunchCommandLine.ExitCode.Errors:
+                    selectedItems.Items[idx].ImageIndex = 3;
+                    //labelResult.Text = "Errors";
+                    break;
+                case LaunchCommandLine.ExitCode.Fatal:
+                    selectedItems.Items[idx].ImageIndex = 4;
+                    //labelResult.Text = "Fatal";
+                    break;
+                default:
+                    selectedItems.Items[idx].ImageIndex = 4;
+                    //labelResult.Text = "Unknown";
+                    break;
+            }
+            selectedItems.EndUpdate();
+
+            progress = step * (idx + 1);
+            progressBar.Value = (int)progress;
+            ActionLabel.Text = progressBar.Value + "% complete";
+            idx++;
+            timer.Stop();
+            elapsed = timer.Elapsed;
+            double TimeRemaining = elapsed.TotalSeconds * (itemsToByProcessed - idx + 1);
+            TimeSpan TimeSpanRemaining = TimeSpan.FromSeconds(TimeRemaining);
+            //String timeStr = TimeSpanRemaining.ToReadableString();
+            //m_timeRemaining = "About " + timeStr;
+
+            ProgressChanged();
+
+            
+            timeRemainingLabel.Text = "Complete";
+            progressBar.Value = 100;
+            ActionLabel.Text = "Complete";
+            return 42;
+        }
+
+
+        // Called on process state only.
+        void OnStatusChanged(LaunchCommandLine.Status param)
+        {
+            switch (param)
+            {
+                case LaunchCommandLine.Status.Running:
+                    break;
+                case LaunchCommandLine.Status.Completed:
+                    //if (StatusChanged != null)
+                    //{
+                    //    StatusChanged(Action.Complete);
+                    //}
+
+                    break;
+                case LaunchCommandLine.Status.Error:
+                    break;
+            }
+        }
+
+
+        void OnProgressChanged()
+        {
+            ActionLabel.Text = progressBar.Value + "% complete";
+            timeRemainingLabel.Text = m_timeRemaining;
+            //this.nameLabel.Text = m_currentFile;
+            Refresh();
+            System.Diagnostics.Debug.WriteLine("My text");
+        }
+
+        internal static class TimeSpanExt
+        {
+            public static string ToReadableString(TimeSpan span)
+            {
+                string formatted = string.Format("{0}{1}{2}{3}",
+                    span.Duration().Days > 0 ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? string.Empty : "s") : string.Empty,
+                    span.Duration().Hours > 0 ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? string.Empty : "s") : string.Empty,
+                    span.Duration().Minutes > 0 ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? string.Empty : "s") : string.Empty,
+                    span.Duration().Seconds > 0 ? string.Format("{0:0} second{1}", span.Seconds, span.Seconds == 1 ? string.Empty : "s") : string.Empty);
+
+                if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+
+                if (string.IsNullOrEmpty(formatted)) formatted = "0 seconds";
+
+                return formatted;
+            }
+        }
+    }
+}
