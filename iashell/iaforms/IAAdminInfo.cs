e69b96b2a7206542b75cf04c5b127233bf3ce935
Iain Ferguson <i.ferguson@idk.co.uk>
2024-11-27 20:18:19 +0000

    added IAAdminInfo.cs

---
+++ iashell/iaforms/IAAdminInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iaforms
+{
+    public class IAAdminInfo
+    {
+        public static bool UnSavedChanges { get; set; }
+        public static string ExePath { get; set; }
+        public static string WorkingPath { get; set; }
+        public static string FilePath { get; set; }
+
+        //public static bool UnSavedChanges
+        //{
+        //    get { return m_UnSavedChanges; }
+        //    set { m_UnSavedChanges = value; }
+        //}
+    }
+}
