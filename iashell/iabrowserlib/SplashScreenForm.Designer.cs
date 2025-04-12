bfb81d0206cc810311031228e11f4f45ec2013c4
Iain Ferguson <i.ferguson@idk.co.uk>
2025-04-12 13:40:07 +0100

    added

---
+++ iashell/iabrowserlib/SplashScreenForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace iabrowserlib
+{
+    partial class SplashScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreenForm));
+            pictureBox1 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(35, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(61, 60);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(69, 359);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(486, 34);
+            progressBar1.TabIndex = 1;
+            // 
+            // SplashScreenForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(613, 450);
+            ControlBox = false;
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SplashScreenForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SplashScreen";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ iashell/iabrowserlib/SplashScreenForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iabrowserlib
+{
+    public partial class SplashScreenForm : Form
+    {
+        public SplashScreenForm()
+        {
+            EventHandler closeForm;
+            //closeForm += CloseForm;
+            InitializeComponent();
+            Thread.Sleep(5000);
+            //closeForm?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void CloseForm(object? sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+
+}
